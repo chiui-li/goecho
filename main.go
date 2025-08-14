3882903fc399e7acafeabee22ef108baa3ddd3cd
chiui <160477438+chiui-li@users.noreply.github.com>
2025-08-14 20:02:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net"
@@ -9,18 +10,33 @@ import (
 )
 
 func handleConn(conn net.Conn) {
-	resHeader := "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nConnection: keep-alive\r\n\r\n"
+	const resHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nConnection: close\r\n\r\n"
 	reqTime := "time:" + time.Now().Format("2006-01-02 15:04:05") + "\n"
-	addr := "HTTP: " + conn.RemoteAddr().String() + "\n"
+	addr := "TCP: " + conn.RemoteAddr().String() + "\n"
 	content := bufio.NewReader(conn)
-	log.Println(reqTime, addr)
 	conn.Write([]byte(resHeader))
 	conn.Write([]byte(reqTime))
 	conn.Write([]byte(addr + "\n"))
-	conn.Write([]byte(reqTime))
-	content.WriteTo(conn)
+	response := []byte{}
+	reqHeaderEndIndex := 0
+	bodyIndex := 0
+	for {
+		responseLine, err := content.ReadBytes('\n')
+		if err != nil {
+			break
+		}
+		response = append(response, responseLine...)
+		if len(response) >= 4 && bodyIndex == 0 && bytes.Equal(response[len(response)-4:], []byte("\r\n\r\n")) {
+			bodyIndex = len(response)
+			reqHeaderEndIndex = bodyIndex - 4
+			conn.Write(response[:reqHeaderEndIndex])
+			break
+		}
+	}
 	tcp, _ := conn.(*net.TCPConn)
 	tcp.CloseWrite()
+	log.Println(addr)
+	log.Println(string(response))
 }
 
 func main() {
